369d5addc87569190131c8b1adf528ca3112d913
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-26 11:20:03 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -47,7 +47,7 @@ def eval(params):
 				y = y.to(params.device)
 				x2, _ = model(x1, y)
 				x1 = torch.nan_to_num(x1, nan=0.0, posinf=1, neginf=-1)
-            	x2 = torch.nan_to_num(x2, nan=0.0, posinf=1, neginf=-1)
+				x2 = torch.nan_to_num(x2, nan=0.0, posinf=1, neginf=-1)
 				s,p,f = ssim(x1.cpu(),x2.cpu()), psnr(x1.cpu(),x2.cpu()),fid_3d(fid_model, x1.cpu(), x2.cpu())
 				ssims.append(s)
 				psnrs.append(p)
